2ea74cbf89cc1a878c726afdc31d5e80483666bc
Petar Maksimovic <petar.maksimovic@runtimeverification.com>
2023-10-21 09:23:49 +0100

    adjustment to test

--- test/FixedPointMathLib.k.sol
+++ test/FixedPointMathLib.k.sol
@@ -32,16 +32,16 @@ contract FixedPointMathLibVerification is Test, KevmUtil {
     function testLog2(uint256 x) public {
 
         unchecked {
-          uint256 y = x; uint256 z = 0;
+          uint256 y = x;
           if (2 ** 128 - 1 < y) { y = y >> 128; } else { }
           if (2 **  64 - 1 < y) { y = y >>  64; } else { }
           if (2 **  32 - 1 < y) { y = y >>  32; } else { }
           if (2 **  16 - 1 < y) { y = y >>  16; } else { }
           if (2 **   8 - 1 < y) { y = y >>   8; } else { }
           if (2 **   4 - 1 < y) { y = y >>   4; } else { }
-          if (2 **   3 - 1 < y) { y = y >>   1; } else { }
-          if (2 **   2 - 1 < y) { y = y >>   1; } else { }
+          if (2 **   2 - 1 < y) { y = y >>   2; } else { }
           if (2 **   1 - 1 < y) { y = y >>   1; } else { }
+          if (2 **   0 - 1 < y) { y = 0;        } else { }
         }
 
         uint256 r = FixedPointMathLib.log2(x);
